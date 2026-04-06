a7780cf49dba2abc6e291a02de9bb799c0a59655
ramteja <ramt77073@gmail.com>
2026-04-06 12:57:09 +0530

    fix api

---
+++ README.md
@@ -0,0 +1,29 @@
+## Live Demo
+frontend: (we will add tomorrow)
+API: https://Student-marks-predictor-4ecf.onrender.com/docs
+
+# Student Marks Predictor
+
+This project predicts student marks based on study hours using Machine Learning.
+
+## Tech Stack
+- Python
+- FastAPI
+- Scikit-learn
+- HTML, CSS, JavaScript
+- Render (Deployment)
+
+## Features
+- Predict marks using ML model
+- API built with FastAPI
+- Frontend UI for user input
+- Live deployment
+
+## Live API
+https://Student-marks-predictor-4ecf.onrender.com/docs
+
+## How it works
+1. User enters stuy hours
+2. Data sent to API
+3. Model Predicts marks
+4. Result displayed on screeen
--- api.py
+++ api.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel
 import joblib
 import pandas as pd
 
@@ -15,19 +16,30 @@ app.add_middleware(
 
 model = joblib.load("marks_model.joblib")
 
+
+class StudentInput(BaseModel):
+    hours: float
+
+
 @app.get("/")
 def home():
     return {"message": "Student Marks Predictor API is running"}
 
+
 @app.get("/about")
 def about():
-    return {"message": "This API predicts student marks based  on study hours"}
+    return {"message": "This API predicts student marks based on study hours"}
+
 
 @app.post("/predict")
-def predict(data: dict):
-    hours = data["hours"]
+def predict(data: StudentInput):
+    hours = data.hours
 
-    prediction = model.predict(pd.DataFrame({"hours": [hours]}))
+    input_df = pd.DataFrame({
+        "hours": [hours]
+    })
+
+    prediction = model.predict(input_df)
 
     predicted_marks = round(float(prediction[0]), 2)
 
