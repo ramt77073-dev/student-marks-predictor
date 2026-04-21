293bbca8110feec229edfd6bc8d075ea16df6966
ramteja <ramt77073@gmail.com>
2026-04-21 14:50:30 +0530

    njds

--- database.py
+++ database.py
@@ -1,12 +1,8 @@
 import os
 from pymongo import MongoClient
 
-MONGO_URL = MONGO_URL = "mongodb://localhost:27017"
-if not MONGO_URL:
-    raise ValueError("MONGO_URL environment variable is not set")
-
+MONGO_URL = os.getenv("MONGO_URL")
 client = MongoClient(MONGO_URL)
 db = client["student_predictor"]
-
 users_collection = db["users"]
 predictions_collection = db["predictions"]
